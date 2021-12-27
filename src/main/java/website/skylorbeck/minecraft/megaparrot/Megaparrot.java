3980c543b4bc5f7eca5b91ba0f66c02bd9f05926
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-12-27 10:01:16 -0800

    Initial commit

---
+++ src/main/java/website/skylorbeck/minecraft/megaparrot/Megaparrot.java
@@ -0,0 +1,10 @@
+package website.skylorbeck.minecraft.megaparrot;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Megaparrot implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/website/skylorbeck/minecraft/megaparrot/client/MegaparrotClient.java
@@ -0,0 +1,13 @@
+package website.skylorbeck.minecraft.megaparrot.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MegaparrotClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
